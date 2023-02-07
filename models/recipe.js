7c2c43c912616c9e6468fa9515b7e04cfb5fce74
Vanessa Bragdon <vrbragdon@gmail.com>
2023-02-06 17:40:38 -0800

    recipe findAll

--- models/recipe.js
+++ models/recipe.js
@@ -8,9 +8,9 @@ class Recipe {
     * 
     * update db, return new recipe data
     * 
-    * data should be { title, cuisine, ingredients list, instructions, notes }
+    * data should be { title, cuisine, ingredients list, instructions, notes, username }
     * 
-    * Returns { id, title, cuisine, ingredients list, instructions, notes }
+    * Returns { id, title, cuisine, ingredients list, instructions, notes, username }
     * 
     * Throws BadRequestError if recipe title already exists
     */
@@ -20,21 +20,47 @@ class Recipe {
                               cuisine, 
                               ingredients, 
                               instructions, 
-                              notes)
-         VALUES ($1, $2, $3, $4, $5)
-         RETURNING id, title, cuisine, ingredients, instructions, notes`,
+                              notes,
+                              username)
+         VALUES ($1, $2, $3, $4, $5, $6)
+         RETURNING id, title, cuisine, ingredients, instructions, notes, username`,
          [
             title,
             cuisine,
             ingredients,
             instructions,
-            notes
+            notes,
+            username
          ],
       );
       const recipe = result.rows[0]
 
       return recipe;
    }
+
+   /** Find all recipes
+    * 
+    * searchFilters?
+    * 
+    * Returns [{title, cuisine, ingredients, instructions, notes, username }]
+    */
+   static async findAll() {
+      const result = await db.query(
+         `SELECT 
+            title,
+            cuisine,
+            ingredients,
+            instructions,
+            notes,
+            username
+         FROM recipes`
+      );
+
+      const recipes = result.rows;
+      return recipes;
+   }
+
+
 }
 
 module.exports = Recipe;
--- routes/recipes.js
+++ routes/recipes.js
@@ -4,7 +4,13 @@ const db = require("../db")
 
 const Recipe = require("../models/recipe")
 
-router.post("/", async function (req, res, next) {
+/** POST / {recipe} => {recipe} 
+ * 
+ * recipe should be { title, cuisine, ingredients, instructions, notes, username }
+ * 
+ * Returns { title, cuisine, ingredients, instructions, notes, username }
+*/
+router.post("/add", async function (req, res, next) {
    try {
       const recipe = await Recipe.create(req.body)
       // console.log(recipe)
@@ -13,5 +19,25 @@ router.post("/", async function (req, res, next) {
       return next(err)
    }
 })
+
+/** GET / => {recipes: [{ title, cuisine, ingredients, instructions, notes, username }...]} 
+*/
+router.get("/", async function (req, res, next) {
+   try {
+      const recipes = await Recipe.findAll()
+      return res.json({ recipes })
+   } catch (err) {
+      return next(err)
+   }
+})
+
+/** GET / {search} => {recipes: [{ title, cuisine, ingredients, instructions, notes, username }...]}  
+ * 
+ * Search filter in query:
+ * - cuisine
+ * - ingredients
+ * - title?
+*/
+
 
 module.exports = router;
